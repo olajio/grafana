465b55cec79b64c3c2076ed85facc6104c037a68
Olamide Olajide <34173234+olajio@users.noreply.github.com>
2026-04-21 11:29:56 -0500

    Delete docs directory

--- docs/examples/image_factory_example-image/_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "image_factory" {
-  description = "Image factory outputs"
-  value       = module.factory
-}
--- docs/examples/image_factory_example-image/_provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
-provider "aws" {
-  alias  = "dr"
-  region = "us-east-1"
-}
